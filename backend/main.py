2e9aa323ac7ceb4e1639a24eac3412f4ab63244f
namlee1998 <longtaiew01@gmail.com>
2025-07-23 17:05:01 +0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -159,5 +159,6 @@ async def websocket_endpoint(websocket: WebSocket):
 
 
 if __name__ == "__main__":
-    uvicorn.run("backend.main:app", host="0.0.0.0", port=8080, workers=1)
+    uvicorn backend.main:app --host 0.0.0.0 --port 8080
+
 
