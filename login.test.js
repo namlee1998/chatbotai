5963653e02db0f84018e5525bcfa5f91d615c68f
namlee1998 <longtaiew01@gmail.com>
2025-07-24 10:50:09 +0700

    Update login.test.js

--- login.test.js
+++ login.test.js
@@ -3,8 +3,13 @@ const fetch = require('node-fetch');
 test('Đăng nhập trả về token', async () => {
   const response = await fetch('http://127.0.0.1:8080/api/login', {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: new URLSearchParams({ username: 'admin', password: 'Namltmta1998' }).toString()
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams({
+      username: 'admin',
+      password: 'Namltmta1998'
+    }).toString()
   });
 
   expect(response.status).toBe(200);
