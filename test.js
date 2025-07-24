afa4b051f63336dc7d7a4f799ad6bd2c304c536d
namlee1998 <longtaiew01@gmail.com>
2025-07-24 10:35:18 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 test('Đăng nhập trả về token', async () => {
-  const response = await fetch('http://localhost:8080/api/login', {
+  const response = await fetch('http://127.0.0.1:8080/api/login', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ username: 'admin', password: '123456' })
