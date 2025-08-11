1f934a6ecbcd4d5b5a53a06abdddc64eae5068e7
namlee1998 <longtaiew01@gmail.com>
2025-08-11 09:37:32 +0700

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from "./LoginPage";
 import ChatPage from "./ChatPage";
 
 function App() {
