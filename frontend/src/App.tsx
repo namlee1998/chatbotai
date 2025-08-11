835c19670795381aca0bb4ba4824a6a62229218b
namlee1998 <longtaiew01@gmail.com>
2025-08-11 09:37:47 +0700

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        
         <Route path="/chat" element={<ChatPage />} />
       </Routes>
     </BrowserRouter>
